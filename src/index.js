704cafbaac5728a98aa7b092fa58888be91677ac
snpr <vaugk13@gmail.com>
2019-08-19 13:27:24 +0500

    Add CreateUserInput

--- src/index.js
+++ src/index.js
@@ -86,9 +86,15 @@ const typeDefs = `
     }
 
     type Mutation {
-      createUser(name: String!, email: String!, age: Int): User!
+      createUser(data: CreateUserInput): User!
       createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
       createComment(text: String!, author: ID!, post: ID!): Comment!
+    }
+
+    input CreateUserInput {
+      name: String!
+      email: String!
+      age: Int
     }
 
     type User {
@@ -162,7 +168,7 @@ const resolvers = {
   },
   Mutation: {
     createUser(parent, args, ctx, info) {
-      const emailTaken = users.some(user => user.email === args.email);
+      const emailTaken = users.some(user => user.email === args.data.email);
 
       if (emailTaken) {
         throw new Error("Email taken.");
@@ -170,7 +176,7 @@ const resolvers = {
 
       const user = {
         id: uuidv4(),
-        ...args
+        ...args.data
       };
 
       users.push(user);
