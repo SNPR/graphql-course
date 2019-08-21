aec9f1961f01316c01110718f79cc01e64a090fc
snpr <vaugk13@gmail.com>
2019-08-21 14:27:52 +0500

    Add subscriptions of the post modification

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -115,9 +115,10 @@ const Mutation = {
 
     return post;
   },
-  updatePost(parent, args, { db }, info) {
+  updatePost(parent, args, { db, pubsub }, info) {
     const { id, data } = args;
     const post = db.posts.find(post => post.id === id);
+    const originalPost = { ...post };
 
     if (!post) {
       throw new Error("Post not found");
@@ -133,8 +134,30 @@ const Mutation = {
 
     if (typeof data.published === "boolean") {
       post.published = data.published;
-    }
 
+      if (originalPost.published && !post.published) {
+        pubsub.publish("post", {
+          post: {
+            mutation: "DELETED",
+            data: originalPost
+          }
+        });
+      } else if (!originalPost.published && post.published) {
+        pubsub.publish("post", {
+          post: {
+            mutation: "CREATED",
+            data: post
+          }
+        });
+      }
+    } else if (post.published) {
+      pubsub.publish("post", {
+        post: {
+          mutation: "UPDATED",
+          data: post
+        }
+      });
+    }
     return post;
   },
   createComment(parent, args, { db, pubsub }, info) {
